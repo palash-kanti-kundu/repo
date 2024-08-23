bf521fe2e2a5ca36bde63c4d7474728aea0bc896
kundupa <palash-kanti.kundu@hpe.com>
2024-08-23 11:39:38 +0530

    Add file write util

---
+++ file_util.py
@@ -0,0 +1,40 @@
+import os
+import time
+from time import strftime, gmtime
+import sys
+from collections import deque
+import logging
+
+capacity = 30
+cache = deque()
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='file_util.log')
+logger = logging.getLogger()
+
+while True:
+    formatted_time = strftime("%Y_%m_%d_%H__%M__%S", gmtime())
+    
+    file_path = "./dummy_files/" + formatted_time
+
+    if len(sys.argv) == 2:
+        size_in_mb = int(sys.argv[1])
+    else:
+        size_in_mb = 10
+	
+    if len(cache) >= capacity:
+	    removed_item = cache.popleft()
+    else:
+         removed_item = None
+    
+    if removed_item is  not None:
+        os.remove(removed_item)
+        logger.info("Removed" + removed_item)
+
+    with open(file_path, 'wb') as file:
+        file.write(os.urandom(size_in_mb * 1024 * 1024))
+        cache.append(file_path)
+
+    logger.info("Written file at " + formatted_time)
+    logger.info("")
+
+    time.sleep(1)
